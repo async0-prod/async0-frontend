706711eed4a899f1bac47f7798072787de6e9199
grvbrk <gouravbarikofficial@gmail.com>
2025-04-24 21:07:13 +0530

    server package moved to its own repo

--- server/src/dependencies/core.py
+++
@@ -1,6 +0,0 @@
-from typing import Annotated
-from src.core.db import get_db_session
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-DBSessionDep = Annotated[AsyncSession, Depends(get_db_session)]
