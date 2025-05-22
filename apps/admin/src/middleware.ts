1369578effd4cc0923931e3fbf39077d4d71ea2e
grvbrk <gouravbarikofficial@gmail.com>
2025-05-22 22:14:29 +0530

    middleware and next-auth config file

---
+++ apps/admin/src/middleware.ts
@@ -0,0 +1,21 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.AUTH_SECRET,
+  });
+  const path = request.nextUrl.pathname;
+
+  if (!token) {
+    // User is not authenticated
+    if (path !== "/login") {
+      return NextResponse.redirect(new URL("/login", request.nextUrl));
+    }
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
