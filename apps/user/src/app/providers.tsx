416947de852e4768268c3f6bf59a9e09000f0072
grvbrk <gouravbarikofficial@gmail.com>
2025-05-23 19:23:39 +0530

    home page ui

--- apps/user/src/app/providers.tsx
+++ apps/user/src/app/providers.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/sonner";
 
 export const Providers = ({ children }: { children: ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <SessionProvider>
       <ThemeProvider
@@ -12,7 +15,14 @@ export const Providers = ({ children }: { children: ReactNode }) => {
         enableSystem
         disableTransitionOnChange
       >
-        {children}
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <Toaster
+            richColors
+            swipeDirections={["right"]}
+            offset={{ bottom: 80 }}
+          />
+        </QueryClientProvider>
       </ThemeProvider>
     </SessionProvider>
   );
