6a76b8e23a1e7daea36e030df8857f092d1eba37
grvbrk <gouravbarikofficial@gmail.com>
2025-08-19 22:53:08 +0530

    Problems table component which houses DataTable

--- apps/user/src/app/problems/_components/ProblemsTable.tsx
+++ apps/user/src/app/problems/_components/ProblemsTable.tsx
@@ -1,9 +1,28 @@
-import { getAllProblems } from "@/app/actions/problems";
+"use client";
+
+import { getAllListProblems } from "@/app/fetch/problem";
 import { columns } from "@/app/problems/columns";
-import { DataTable } from "@/app/problems/data-table";
+import { DataTable } from "@/app/problems/_components/DataTable";
+import { useQuery } from "@tanstack/react-query";
 
-export default async function ProblemsTable() {
-  const problems = await getAllProblems();
+export default function ProblemsTable({
+  selectedListID,
+  handleRowClick,
+}: {
+  selectedListID: string | undefined;
+  handleRowClick: (slug: string, name: string) => void;
+}) {
+  const { data: problems } = useQuery({
+    queryKey: ["problems", selectedListID],
+    queryFn: () => getAllListProblems(selectedListID),
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  });
 
-  return <DataTable columns={columns} data={problems ?? []} />;
+  return (
+    <DataTable
+      columns={columns}
+      data={problems?.data ?? []}
+      handleRowClick={handleRowClick}
+    />
+  );
 }
